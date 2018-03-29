d8d850a8c722ad5bd9f3684ec34f02dd9d823351
DeadSkill <37362922+DeadSkill@users.noreply.github.com>
2018-03-29 12:53:42 +0200

    Big challenge

---
+++ classement_baby/04-projet-php/choix.php
@@ -0,0 +1,59 @@
+
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Baby Squad</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- Custom styles for this template -->
+    <link href="css/cover.css" rel="stylesheet">
+  </head>
+
+  <body class="text-center">
+
+    <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+      <header class="masthead mb-auto">
+        <div class="inner">
+          <!-- <h3 class="masthead-brand">Baby Squad</h3> -->
+          <!-- <nav class="nav nav-masthead justify-content-center">
+            <a class="nav-link active" href="index.php">Accueil</a>
+            <a class="nav-link" href="commence-maintenant.php">Commence maintenant</a>
+            <a class="nav-link" href="contact.php">Contact</a>
+          </nav> -->
+        </div>
+      </header>
+
+      <main role="main" class="inner cover">
+        <h1 class="cover-heading">Fais ton choix.</h1>
+        <p class="lead">Tu as un choix très difficile à faire...</p>
+        <p class="lead">
+          <a href="commence-maintenant-1v1.php" class="btn btn-lg btn-secondary">1 vs 1</a>
+          <a href="commence-maintenant-2v2.php" class="btn btn-lg btn-secondary">2 vs 2</a>
+        </p>
+            <a href="index.php" type="button" class="btn btn-danger">Retour</a>
+      </main>
+
+      <footer class="mastfoot mt-auto">
+        <div class="inner">
+          <p>Projet réalisé par Yanisse YOUMI et Corrantin SPEE</a>.</p>
+        </div>
+      </footer>
+    </div>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="../../../../assets/js/vendor/popper.min.js"></script>
+    <script src="../../../../dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ classement_baby/04-projet-php/contact.php
@@ -0,0 +1,71 @@
+
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Baby Squad</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- Custom styles for this template -->
+    <link href="css/cover.css" rel="stylesheet">
+  </head>
+
+  <body class="text-center">
+
+    <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+      <header class="masthead mb-auto">
+        <div class="inner">
+          <h3 class="masthead-brand">Baby Squad</h3>
+          <nav class="nav nav-masthead justify-content-center">
+            <a class="nav-link" href="index.php">Accueil</a>
+            <a class="nav-link" href="creer-joueur.php">Créer un joueur</a>
+            <a class="nav-link" href="commence-maintenant.php">Commence maintenant</a>
+            <a class="nav-link active" href="contact.php">Contact</a>
+          </nav>
+        </div>
+      </header>
+
+      <main role="main" class="inner cover">
+        <h1 class="cover-heading">Une demande à faire ?</h1>
+        <p class="lead">Un bug? Une demande ? Une recommandation ? Tu es à la bonne adresse !.</p>
+        <form>
+            <div class="form-group">
+              <label for="formGroupExampleInput">Nom</label>
+              <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Rentre ton nom ici">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Adresse email</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Entre ton adresse mail ici">
+                <small id="emailHelp" class="form-text text-muted">Nous ne partagerons pas votre adresse mail.</small>
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlTextarea1">Message</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Fais nous part de ton plus beau message !"></textarea>
+            </div>
+                <button type="submit" class="btn btn-primary">Envoyer !</button>
+</form>
+      </main>
+
+      <footer class="mastfoot mt-auto">
+        <div class="inner">
+          <p>Projet réalisé par Yanisse YOUMI et Corrantin SPEE</a>.</p>
+        </div>
+      </footer>
+    </div>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="../../../../assets/js/vendor/popper.min.js"></script>
+    <script src="../../../../dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
